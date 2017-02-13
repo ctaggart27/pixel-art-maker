a497e7190c1f531d8e3f94cd6c13ce9240407324
ctaggart27 <ctaggart27@gmail.com>
2017-02-13 11:53:14 -0700

    surge changes my formatting for some reason :(

--- pixel.js
+++ pixel.js
@@ -1,42 +1,36 @@
-let body = document.getElementsByTagName('body')[0]
+let body = document.getElementsByTagName('html')[0]
 let pixel = document.getElementsByClassName('pixel')
 
 let boxContainer = document.createElement('div')
 let container = boxContainer.className = "container"
-boxContainer.style.height = '50%'
-boxContainer.style.width = '50%'
-boxContainer.display = 'inline-block'
+boxContainer.style.height = '300px'
+boxContainer.style.width = '750px'
 body.appendChild(boxContainer)
 
 let palette = document.createElement('div')
 palette.id = "palette"
 palette.style.className = "palette"
-palette.style.display = "block"
-palette.style.position = "absolute"
-palette.style.height = '25%'
-palette.style.width = '25%'
-palette.style.right = '1em'
-palette.style.top = '45em'
-palette.style.paddingRight = '10em;'
-palette.style.paddingTop = '45em'
-palette.style.marginTop = '-40em'
+// palette.style.position = "absolute"
+palette.style.height = '750px';
+palette.style.width = '200px';
+palette.style.padding = "20px"
+palette.style.marginTop = '-170px'
 palette.style.backgroundColor = 'rgb(255, 230, 96)'
-palette.style.borderRadius = '10%   20% 60% 30% '
+palette.style.borderRadius = '10%  20% 60% 30% '
 palette.style.border = '10px solid black'
+palette.style.float = "right"
 body.appendChild(palette)
 
-for (let i = 0; i < 810; i++) {
+for (let i = 0; i < 700; i++) {
     let div = document.createElement('div')
-    div.style.height = '.5em'
-    div.style.width = '.5em'
-    div.style.border = '1%'
-    div.style.borderRadius = '5%'
+    div.style.height = '20px'
+    div.style.width = '20px'
+    div.style.border = '1px'
     div.style.borderColor = 'black'
     div.style.borderStyle = 'inset'
     div.style.borderCollapse = 'separate'
-    div.style.padding = '1% 1% 1% 1%'
-    div.style.margin = '0%'
-    div.style.float = 'left'
+    div.style.padding = "5px"
+    div.style.display = "inline-block"
     let pixel = div.className = "pixel"
     boxContainer.appendChild(div)
 }
@@ -45,34 +39,40 @@ for (var i = 0; i < 11; i++) {
     let div = document.createElement('div')
     let colorValue = ['maroon', 'red', 'orange', 'yellow', 'lime', 'green', 'blue', 'violet', 'purple', 'black', 'white']
     let id = div.id = colorValue[i]
-    div.style.height = '1em'
-    div.style.width = '1em'
+    div.style.height = '100px'
+    div.style.width = '75px'
     div.style.border = '2px solid black'
     div.style.display = 'inline-block'
-    div.style.padding = '3em'
-    div.style.margin = '-90% 2% 95% 5%'
+    div.style.padding = '3px'
+    // div.style.position = 'relative'
+    // div.style.margin = '-90% 0% 95% 5%'
     div.style.borderRadius = '75% 30% 80% 75%'
     div.style.backgroundColor = colorValue[i]
     palette.appendChild(div)
 }
 
+
+
+
+
 palette.addEventListener("click", function() {
     let coloring = event.target.style.backgroundColor
-
     let alert = document.createElement('div')
-    alert.style.position = "absolute"
     alert.style.backgroundImage = 'url("https://media.giphy.com/media/d31vTpVi1LAcDvdm/giphy.gif")'
     alert.style.backgroundRepeat = 'no-repeat'
-    alert.style.height = '100%'
-    alert.style.width = '100%'
-    alert.style.bottom = '-100%'
-    alert.style.left = '20%'
+    alert.style.height = '500px'
+    alert.style.width = '500px'
+    alert.style.position = "relative"
+    alert.style.marginLeft = "-700px"
 
     let msg = document.createElement('h1')
     msg.innerText = "You picked  " + coloring + " now paint me some happy trees"
-    msg.style.marginTop = "-43em"
-    palette.appendChild(alert)
+    msg.style.position = "relative"
+    msg.style.right = "300px"
+    msg.style.display = "inline-block"
+
     palette.appendChild(msg)
+    palette.appendChild(alert)
 
     boxContainer.addEventListener('mousedown', function() {
         event.target.style.backgroundColor = coloring
